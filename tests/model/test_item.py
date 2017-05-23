b459f9895906048aaee67fa8246e7673fb874250
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2017-05-23 18:06:35 -0300

    Item com objeto description

--- tests/model/test_item.py
+++ tests/model/test_item.py
@@ -1,17 +1,21 @@
-from model.item import Item
+from model.description import Description
 import pytest
 
 
-def test_item_title(feed):
-    assert Item(feed.items[0]).title == "Fiat Argo é flagrado completamente sem camuflagem"
+@pytest.fixture
+def item(feed):
+    return feed.items[0]
 
 
-def test_item_link(feed):
+def test_item_title(item):
+    assert item.title == "Fiat Argo é flagrado completamente sem camuflagem"
+
+
+def test_item_link(item):
     link = "http://revistaautoesporte.globo.com/Noticias/" \
            "noticia/2017/05/fiat-argo-e-flagrado-completamente-sem-camuflagem.html"
-    assert Item(feed.items[0]).link == link
+    assert item.link == link
 
 
-@pytest.mark.skip(reason="Tem que definir o objeto description")
-def test_item_description(feed):
-    pass
+def test_item_description(item):
+    assert isinstance(item.description, Description)
