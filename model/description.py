d1978a35234da8455f1094bcc25c363fc9e32b41
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2017-05-24 18:04:51 -0300

    Parse das tag de html do description

--- model/description.py
+++ model/description.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from util import convert_to_double_quotes, parse_tag
+from util import parse_tag
 
 
 class Description:
@@ -7,5 +7,4 @@ class Description:
         self.html = BeautifulSoup(tag.text, "html.parser")
 
     def parse(self):
-        return convert_to_double_quotes("'description':{}"
-                                        .format(parse_tag(self.html)))
+        return parse_tag(self.html)
--- tests/model/test_description.py
+++ tests/model/test_description.py
@@ -1,8 +1,5 @@
 from bs4 import BeautifulSoup
 import pytest
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 @pytest.fixture
@@ -19,11 +16,9 @@ def test_text_html(description):
 
 
 def test_parse(description):
-    result = '"description":' \
-             '[{"type": "image", "content": "SRC_IMAGE.jpg"}, ' \
-             '{"type": "text", "content": "TEXTO P A STRONG"}, ' \
-             '{"type": "links", "content": ' \
-             '["LINK_LI_1", "LINK_LI_2", "LINK_LI_3"]}, ' \
-             '{"type": "text", "content": "STRONG A TEXTO A"}, ' \
-             '{"type": "text", "content": "TEXTO P A TEXTO A STRONG"}]'
+    result = [{"type": "image", "content": "SRC_IMAGE.jpg"},
+             {"type": "text", "content": "TEXTO P A STRONG"},
+             {"type": "links", "content": ["LINK_LI_1", "LINK_LI_2", "LINK_LI_3"]},
+             {"type": "text", "content": "STRONG A TEXTO A"},
+             {"type": "text", "content": "TEXTO P A TEXTO A STRONG"}]
     assert description.parse() == result
