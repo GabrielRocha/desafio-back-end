2ec50dde6c5494dbe0783436b9d22fd88c6a0d20
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2017-05-24 18:03:57 -0300

    Adicao de um XML base para testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 @pytest.fixture(scope='session', autouse=True)
 def feed():
     ''' Return CrawlerFeed Object with url test'''
-    url = "file:///{}/xmls/feed.xml".format(BASE_DIR)
+    url = "file:///{}/xml/feed.xml".format(BASE_DIR)
     return CrawlerFeed(url)
 
 
