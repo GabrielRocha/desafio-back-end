e7758713a9bee94e94952f6ea294a800ff9bfcd4
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2017-05-25 12:18:58 -0300

    Settings para webservice

---
+++ settings.py
@@ -0,0 +1,9 @@
+import peewee
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+CONNECTION = peewee.SqliteDatabase('{}/db/desafio.db'.format(BASE_DIR), threadlocals=True)
+
+URL_FEED = "http://revistaautoesporte.globo.com/rss/ultimas/feed.xml"
---
+++ tests/test_settings.py
@@ -0,0 +1,24 @@
+from peewee import SqliteDatabase
+import settings
+import pytest
+import re
+
+
+@pytest.mark.parametrize("key", ["URL_FEED",
+                                  "CONNECTION"])
+def test_key_settings(key):
+    assert key in settings.__dict__
+
+
+@pytest.mark.parametrize("key", ["URL_FEED",
+                                  "CONNECTION"])
+def test_is_not_none_key(key):
+    assert getattr(settings, key)
+
+
+def test_connection_is_peewee_database():
+    assert isinstance(settings.CONNECTION, SqliteDatabase)
+
+
+def test_url_feed_is_http_link():
+    assert re.match(u'^http:\/\/', settings.URL_FEED)
